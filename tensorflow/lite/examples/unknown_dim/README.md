cbd17b8da9543bbc24e371571d3c6c876851beb1
eric <hyunsik.yoon.1024@gmail.com>
2020-04-26 22:38:39 +0900

    fixing unknown_dim.cc

--- tensorflow/lite/examples/unknown_dim/README.md
+++ tensorflow/lite/examples/unknown_dim/README.md
@@ -1,2 +1,11 @@
 
-see https://colab.research.google.com/drive/1gs9xLfsyKe17DchtqiKT9f_ROSxamjLi?authuser=1#scrollTo=4UPMrylPhsqm for converting script
+
+- Converting pbtxt
+    - See https://colab.research.google.com/drive/1gs9xLfsyKe17DchtqiKT9f_ROSxamjLi?authuser=1#scrollTo=4UPMrylPhsqm for converting script
+
+- Converting pb to tflite
+    - `$ docker run -v /home/eric:/home/eric -it -w $(pwd) tensorflow/tensorflow:nightly python3 convert.py`
+
+- Build
+    - `$ bazel build -c opt //tensorflow/lite/examples/unknown_dim:unknown_dim`
+
--- tensorflow/lite/examples/unknown_dim/unknown_dim.cc
+++ tensorflow/lite/examples/unknown_dim/unknown_dim.cc
@@ -1,4 +1,5 @@
 /* Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+   Copyright 2020 Hyun Sik Yoon. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,14 +19,6 @@ limitations under the License.
 #include "tensorflow/lite/model.h"
 #include "tensorflow/lite/optional_debug_tools.h"
 
-// This is an example that is minimal to read a model
-// from disk and perform inference. There is no data being loaded
-// that is up to you to add as a user.
-//
-// NOTE: Do not add any dependencies to this that cannot be built with
-// the minimal makefile. This example must remain trivial to build with
-// the minimal build tool.
-//
 // Usage: minimal <tflite model>
 
 using namespace tflite;
@@ -38,7 +31,7 @@ using namespace tflite;
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
-    fprintf(stderr, "minimal <tflite model>\n");
+    fprintf(stderr, "unknown_dim <tflite model>\n");
     return 1;
   }
   const char* filename = argv[1];
@@ -55,21 +48,111 @@ int main(int argc, char* argv[]) {
   builder(&interpreter);
   TFLITE_MINIMAL_CHECK(interpreter != nullptr);
 
+  int base_index = 0;
+
+  // This is [-1, -1] input
+  // If this is not called, internal shape is set to scalar and error happens.
+  {
+    TfLiteQuantizationParams quant;
+    interpreter->SetTensorParametersReadWrite(
+        0, kTfLiteFloat32, "Placeholder", {2, 3}, quant);
+  }
+  /*
+  These lines are not needed to be called
+  interpreter->SetTensorParametersReadWrite(
+      1, kTfLiteInt32, "Placeholder_1", {2}, quant);
+
+  interpreter->SetTensorParametersReadWrite(
+      2, kTfLiteFloat32, "Relu", {}, quant);
+  */
+
   // Allocate tensor buffers.
   TFLITE_MINIMAL_CHECK(interpreter->AllocateTensors() == kTfLiteOk);
   printf("=== Pre-invoke Interpreter State ===\n");
   tflite::PrintInterpreterState(interpreter.get());
 
   // Fill input buffers
-  // TODO(user): Insert code to fill input tensors
+  printf("=== Input value => ");
+  auto input = interpreter->typed_tensor<float>(0);
+  for (int i = 0; i < 6; i++) {
+    input[i] = i-3;
+    printf("%f, ", input[i]);
+  }
+  printf("\n");
+
+  // fill new_sizes
+  printf("=== new shape of RESHAPE op => ");
+  auto new_shape_input = interpreter->typed_tensor<int>(1);
+  new_shape_input[0] = 1;
+  new_shape_input[1] = 6;
+  printf("%d, %d\n", new_shape_input[0], new_shape_input[1]);
 
   // Run inference
   TFLITE_MINIMAL_CHECK(interpreter->Invoke() == kTfLiteOk);
-  printf("\n\n=== Post-invoke Interpreter State ===\n");
+  printf("=== Post-invoke Interpreter State ===\n");
   tflite::PrintInterpreterState(interpreter.get());
 
+  // output shape
+  int tensor_ind = 0;
+  printf("=== tensor list:\n");
+  for(auto &tensor: interpreter->primary_subgraph().tensors())
+  {
+    printf("\t - tensor #%d: [", tensor_ind++);
+    auto dims = tensor.dims;
+    for (int i = 0; i < dims->size; i++)
+      printf("%d,", dims->data[i]);
+    printf("]\n");
+  }
+
   // Read output buffers
-  // TODO(user): Insert getting data out code.
+  printf("=== Output value => ");
+  auto output = interpreter->typed_tensor<float>(2);
+  for (int i = 0; i < 6; i++) {
+    printf("%f, ", output[i]);
+  }
+  printf("\n\nDone.");
 
   return 0;
+}
+
+void PrintTfLiteIntVector(const TfLiteIntArray* v) {
+  if (!v) {
+    printf(" (null)\n");
+    return;
+  }
+  for (int k = 0; k < v->size; k++) {
+    printf(" %d", v->data[k]);
+  }
+  printf("\n");
+}
+
+void print_op_tensor(Interpreter *interpreter)
+{
+  // TODO(@hyunsik.yoon) need to modify
+  for (size_t node_index = 0; node_index < interpreter->nodes_size();
+       node_index++) {
+    const std::pair<TfLiteNode, TfLiteRegistration>* node_and_reg =
+        interpreter->node_and_registration(static_cast<int>(node_index));
+    const TfLiteNode& node = node_and_reg->first;
+    const TfLiteRegistration& reg = node_and_reg->second;
+    if (reg.custom_name != nullptr) {
+      printf("Node %3zu Operator Custom Name %s\n", node_index,
+             reg.custom_name);
+    } else {
+      printf("Node %3zu Operator Builtin Code %3d %s\n", node_index,
+             reg.builtin_code, EnumNamesBuiltinOperator()[reg.builtin_code]);
+    }
+    printf("  Inputs:");
+    PrintTfLiteIntVector(node.inputs);
+    printf("  Outputs:");
+    PrintTfLiteIntVector(node.outputs);
+    if (node.intermediates && node.intermediates->size) {
+      printf("  Intermediates:");
+      PrintTfLiteIntVector(node.intermediates);
+    }
+    if (node.temporaries && node.temporaries->size) {
+      printf("  Temporaries:");
+      PrintTfLiteIntVector(node.temporaries);
+    }
+  }
 }
